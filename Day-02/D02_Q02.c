980dcf3af363e9bf4ad3c29d4dd741bc50f85647
Manas Kumar <manaskr501@gmail.com>
2026-09-16 09:57:35 +0530

    Add GCD calculation functionality in D02_Q02.c
    Implement a recursive function to calculate the GCD of two integers, including user input and output.

---
+++ Day-02/D02_Q02.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int findGCD(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+
+    return findGCD(b, a % b);
+}
+
+int main() {
+    int num1, num2, gcd;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    gcd = findGCD(num1, num2);
+
+    printf("The GCD of %d and %d is: %d\n", num1, num2, gcd);
+
+    return 0;
+}
